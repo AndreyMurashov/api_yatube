6689b8fe366158a41870b4bbb25addbb0e7b2e5c
Andrey Murashov <amourashov@yandex.ru>
2024-02-18 10:59:46 +0300

    после ревью

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -9,7 +9,7 @@ router_v1.register('posts', PostViewSet, basename='posts')
 router_v1.register('groups', GroupViewSet, basename='groups')
 router_v1.register('users', UserViewSet, basename='users')
 router_v1.register(r'posts/(?P<post_id>\d+)/comments',
-                CommentViewSet, basename='comments')
+                   CommentViewSet, basename='comments')
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
